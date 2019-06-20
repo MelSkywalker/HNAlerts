6ccf32fea42185d2d860d39fff333e256a43a57f
Melisa Quesada <melisa.quc@gmail.com>
2019-06-20 13:32:34 -0500

    Find all the news with Google and Fb topics

---
+++ configFb.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: process.env.TWITTER_GOOGLE_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_GOOGLE_COMSUMER_SECRET,
+    access_token_key: process.env.TWITTER_GOOGLE_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_GOOGLE_ACCESS_TOKEN_SECRET
+}
--- index.js
+++ index.js
@@ -2,19 +2,23 @@ require('dotenv').config();
 const cheerio = require('cheerio');
 const Twitter = require('twitter');
 const fetch = require('node-fetch');
-const config = require('./config')
+const configGoogle = require('./configGoogle')
+const configFb = require('./configFb');
 const cron = require('node-cron');
+const flatten = require('lodash.flatten');
 
 const url = 'https://news.ycombinator.com/news';
-const topic = process.argv[2];
-const user = process.argv[3];
+const topics = ['Google', 'Firefox'];
+const prevNews = [];
 
-const userExists = (u) => {
-    if (u) return `@${u} `;
-    else return '';
+// const twitter = new Twitter(configGoogle);
+const twitterGoogle = new Twitter(configGoogle);
+const twitterFacebook = new Twitter(configFb);
+
+const twitters = {
+    Google: twitterGoogle,
+    Firefox: twitterFacebook
 }
-
-const twitter = new Twitter(config);
 
 function getNews() {
     return fetch(`${url}`)
@@ -32,44 +36,43 @@ const applyCheerio = body => {
     return news;
 }
 
-const filteredNotes = (res) => {
-    return res.filter(note => {
-        return note.title.includes(topic);
-    })
-};
+const filterNotes = (news, topics) => flatten(topics.map(topic => {
+    return news.filter(note => note.title.includes(topic)).map(note => ({...note, topic}));
+}));
 
-const getLink = notes => {
-    return notes[0].link;
-};
-
-
-const tweetResult = (res) => {
-    return new Promise(function (resolve, reject) {
-        twitter.post('statuses/update', {status: `¡Hola${(userExists(user))}! Esta es la última noticia de #${topic}: ${res} `}, (function(error, tweet, response) {
+const tweetNotes = (notes) => {
+    return notes.map(note => console.log(note.topic) || new Promise(function(resolve, reject) {
+        twitters[note.topic].post('statuses/update', {status: `#hn${note.topic}Alerts Esta es la última noticia de #${note.topic}: ${note.link}`}, (function(error, tweet, response) {
             if(error !== null) {
                 reject(error);
             }
-            resolve(tweet, console.log('Tweeted!'));
+            resolve(tweet, console.log(`Tweeted on ${note.topic} news!`));
         }))
-    })
-};
+    }))
+}
 
 const showErrors = (error) => {
     console.error(error);
 };
 
+const handleGoogleNotes = (res) => filterNotes(res, topics);
+
+const checkTweets = () => {
+
+}
+
 const tweetNews = () => {
     getNews()
         .then(applyCheerio)
-        .then(filteredNotes)
-        .then(getLink)
-        .then(tweetResult)
+        .then(handleGoogleNotes)
+        .then(tweetNotes)
+        .then((res) => (Promise.all(res)))
         .catch(showErrors);
 }
 
 const cronNews = () => {
     tweetNews();
-    cron.schedule('0 */3 * * *', function() {
+    cron.schedule('0 */1 * * *', function() {
         tweetNews();
     })
 }
